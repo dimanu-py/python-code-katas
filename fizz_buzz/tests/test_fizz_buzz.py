5a7c515aead00f70a3042e996bee754d1936a3dc
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:44:45 +0200

    feat(fizz-buzz): add functionality when number is divisible by 3 and 5
    This could violate the baby steps principle, but considering at this point we now the domain and have experienced solving the problem, we can consider this as a baby step with the context we have.

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -16,3 +16,8 @@ def test_return_fizz_when_divisible_by_3(number: int, expected: str):
 @pytest.mark.parametrize("number, expected", [(5, "Buzz"), (10, "Buzz"), (20, "Buzz"), (25, "Buzz"), (35, "Buzz")])
 def test_return_buzz_when_divisible_by_5(number: int, expected: str):
     assert fizz_buzz(number) == expected
+
+
+@pytest.mark.parametrize("number, expected", [(15, "FizzBuzz"), (30, "FizzBuzz"), (45, "FizzBuzz"), (60, "FizzBuzz"), (75, "FizzBuzz")])
+def test_return_fizzbuzz_when_divisible_by_3_and_5(number: int, expected: str):
+    assert fizz_buzz(number) == expected
