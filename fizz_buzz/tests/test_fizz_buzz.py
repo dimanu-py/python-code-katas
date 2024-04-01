4184dbfb1b4bbe970f2cbdfcbf2e31785fc31a74
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:27:59 +0200

    refactor(fizz-buzz): parametrize tests

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -1,9 +1,8 @@
+import pytest
+
 from fizz_buzz.src.fizz_buzz import fizz_buzz
 
 
-def test_return_string_when_non_divisible_number():
-    assert fizz_buzz(1) == "1"
-
-
-def test_return_string_when_non_divisible_number_2():
-    assert fizz_buzz(2) == "2"
+@pytest.mark.parametrize("number, expected", [(1, "1"), (2, "2")])
+def test_return_string_when_non_divisible_number(number: int, expected: str):
+    assert fizz_buzz(number) == expected
