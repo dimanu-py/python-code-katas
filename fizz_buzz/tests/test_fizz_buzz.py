a1df9adddf123a32e453ec417b592176de5f53b8
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:27:32 +0200

    feat(fizz-buzz): when a single number different than one is passed, it returns the same as a string

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -3,3 +3,7 @@ from fizz_buzz.src.fizz_buzz import fizz_buzz
 
 def test_return_string_when_non_divisible_number():
     assert fizz_buzz(1) == "1"
+
+
+def test_return_string_when_non_divisible_number_2():
+    assert fizz_buzz(2) == "2"
