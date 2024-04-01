8af1390f0de9084c9abad64e35141bb4cc9b0f11
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:35:33 +0200

    feat(fizz-buzz): return Fizz when number is 3

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -6,3 +6,7 @@ from fizz_buzz.src.fizz_buzz import fizz_buzz
 @pytest.mark.parametrize("number, expected", [(1, "1"), (2, "2"), (4, "4")])
 def test_return_string_when_non_divisible_number(number: int, expected: str):
     assert fizz_buzz(number) == expected
+
+
+def test_return_fizz_when_divisible_by_3():
+    assert fizz_buzz(3) == "Fizz"
