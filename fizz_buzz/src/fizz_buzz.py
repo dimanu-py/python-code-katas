db6d40c79a4457958810f942d920ac6760c26d04
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:44:45 +0200

    feat(fizz-buzz): add functionality when number is divisible by 3 and 5
    This could violate the baby steps principle, but considering at this point we now the domain and have experienced solving the problem, we can consider this as a baby step with the context we have.

--- fizz_buzz/src/fizz_buzz.py
+++ fizz_buzz/src/fizz_buzz.py
@@ -1,6 +1,8 @@
 
 
 def fizz_buzz(number: int) -> str:
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
     if number % 3 == 0:
         return "Fizz"
     if number % 5 == 0:
