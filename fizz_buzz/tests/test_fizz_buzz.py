dd0697d09e0c2ba5978616587bc86262c1e303e4
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:37:07 +0200

    feat(fizz-buzz): support fizz when number is 6

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -8,5 +8,6 @@ def test_return_string_when_non_divisible_number(number: int, expected: str):
     assert fizz_buzz(number) == expected
 
 
-def test_return_fizz_when_divisible_by_3():
-    assert fizz_buzz(3) == "Fizz"
+@pytest.mark.parametrize("number, expected", [(3, "Fizz"), (6, "Fizz")])
+def test_return_fizz_when_divisible_by_3(number: int, expected: str):
+    assert fizz_buzz(number) == expected
