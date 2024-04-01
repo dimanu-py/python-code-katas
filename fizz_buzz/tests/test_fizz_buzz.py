5bcbe7c930701ec393828ca2117707926091745e
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:40:17 +0200

    feat(fizz-buzz): return buzz when number is 5

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -11,3 +11,8 @@ def test_return_string_when_non_divisible_number(number: int, expected: str):
 @pytest.mark.parametrize("number, expected", [(3, "Fizz"), (6, "Fizz"), (9, "Fizz")])
 def test_return_fizz_when_divisible_by_3(number: int, expected: str):
     assert fizz_buzz(number) == expected
+
+
+@pytest.mark.parametrize("number, expected", [(5, "Buzz")])
+def test_return_buzz_when_divisible_by_5(number: int, expected: str):
+    assert fizz_buzz(number) == expected
