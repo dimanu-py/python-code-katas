05ca09663e3469c38481f9dc9c1fe8335baffe81
dimanu-py <diegomtz126@gmail.com>
2024-04-01 20:29:38 +0200

    feat(fizz-buzz): add support for different non divisible number

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -3,6 +3,6 @@ import pytest
 from fizz_buzz.src.fizz_buzz import fizz_buzz
 
 
-@pytest.mark.parametrize("number, expected", [(1, "1"), (2, "2")])
+@pytest.mark.parametrize("number, expected", [(1, "1"), (2, "2"), (4, "4")])
 def test_return_string_when_non_divisible_number(number: int, expected: str):
     assert fizz_buzz(number) == expected
