5d5ab368860c9a758b63a08a42ff470c17043e47
dimanu-py <diegomtz126@gmail.com>
2024-04-02 09:33:25 +0200

    refactor(leap-year): encapsulate functionality within class
    If we noticed, we are passing constantly the information of the year, it makes sense to share it inside a class

--- leap_year/src/leap_year.py
+++ leap_year/src/leap_year.py
@@ -1,13 +1,16 @@
 
 
-def leap_year(year_number: int) -> bool:
+class Year:
 
-    return is_divisible_by(4, year_number) and (is_not_divisible_by(100, year_number) or is_divisible_by(400, year_number))
+    def __init__(self, year_number: int) -> None:
+        self.year_number = year_number
 
+    def is_leap_year(self) -> bool:
 
-def is_not_divisible_by(divisor: int, year_number: int) -> bool:
-    return year_number % divisor != 0
+        return self.is_divisible_by(4) and (self.is_not_divisible_by(100) or self.is_divisible_by(400))
 
+    def is_not_divisible_by(self, divisor: int) -> bool:
+        return self.year_number % divisor != 0
 
-def is_divisible_by(divisor: int, year: int) -> bool:
-    return year % divisor == 0
+    def is_divisible_by(self, divisor: int) -> bool:
+        return self.year_number % divisor == 0
