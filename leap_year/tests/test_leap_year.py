db4378df475133fea09571a2098604d7c9b46ca8
dimanu-py <diegomtz126@gmail.com>
2024-04-02 09:03:39 +0200

    refactor(leap-year): detect correctly leap year when is divisible by 4

--- leap_year/tests/test_leap_year.py
+++ leap_year/tests/test_leap_year.py
@@ -7,6 +7,6 @@ def test_is_not_leap_year():
     assert leap_year(2015) == False
 
 
-@pytest.mark.parametrize("year_number", [2016, 2020])
+@pytest.mark.parametrize("year_number", [2016, 2020, 2024])
 def test_leap_year_divisible_by_4(year_number: int):
     assert leap_year(year_number) == True
