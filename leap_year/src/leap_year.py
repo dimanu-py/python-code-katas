f0e6b4ebdc5b10702ce8f633567016d36ada2850
dimanu-py <diegomtz126@gmail.com>
2024-04-02 09:23:29 +0200

    refactor(leap-year): turn nested conditions into one liner
    A year is leap if is divisible by 4 and not by 100, or if is divisible by 400

--- leap_year/src/leap_year.py
+++ leap_year/src/leap_year.py
@@ -1,8 +1,5 @@
 
 
 def leap_year(year_number: int) -> bool:
-    if year_number % 4 == 0:
-        if year_number % 100 == 0:
-            return year_number % 400 == 0
-        return True
-    return False
+
+    return year_number % 4 == 0 and (year_number % 100 != 0 or year_number % 400 == 0)
