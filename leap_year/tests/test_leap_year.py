0cb01e7fe60deb4e882e26d33118648dccdf237c
dimanu-py <diegomtz126@gmail.com>
2024-04-02 09:01:20 +0200

    feat(leap-year): fake implementation for leap year when is divisible by four

--- leap_year/tests/test_leap_year.py
+++ leap_year/tests/test_leap_year.py
@@ -3,3 +3,7 @@ from leap_year.src.leap_year import leap_year
 
 def test_is_not_leap_year():
     assert leap_year(2015) == False
+
+
+def test_leap_year_divisible_by_4():
+    assert leap_year(2016) == True
