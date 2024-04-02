a5b6103074010140b1f640be3b9ef94f3e045199
dimanu-py <diegomtz126@gmail.com>
2024-04-02 09:19:43 +0200

    test(leap-year): check that leap year is detected when is divisible by 100 and 400

--- leap_year/tests/test_leap_year.py
+++ leap_year/tests/test_leap_year.py
@@ -16,3 +16,8 @@ def test_leap_year_divisible_by_4(year_number: int):
 @pytest.mark.parametrize("year_number", [1700, 1800, 1900, 2100, 2200])
 def test_is_not_leap_year_divisible_by_100_but_not_by_400(year_number: int):
     assert leap_year(year_number) == False
+
+
+@pytest.mark.parametrize("year_number", [1600, 2000, 2400, 2800, 3200])
+def test_leap_year_divisible_by_100_and_400(year_number: int):
+    assert leap_year(year_number) == True
