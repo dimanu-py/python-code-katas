43c0511c8e3aff55805a73de70d6ac35c775a0f6
dimanu-py <diegomtz126@gmail.com>
2024-04-02 09:27:24 +0200

    refactor(leap-year): extract functions to improve readability and remove duplication

--- leap_year/src/leap_year.py
+++ leap_year/src/leap_year.py
@@ -2,4 +2,12 @@
 
 def leap_year(year_number: int) -> bool:
 
-    return year_number % 4 == 0 and (year_number % 100 != 0 or year_number % 400 == 0)
+    return is_divisible_by(4, year_number) and (is_not_divisible_by(100, year_number) or is_divisible_by(400, year_number))
+
+
+def is_not_divisible_by(divisor: int, year_number: int) -> bool:
+    return year_number % divisor != 0
+
+
+def is_divisible_by(divisor: int, year: int) -> bool:
+    return year % divisor == 0
