aa5f4157b73156747564435dbe6df28fa867ab89
dimanu-py <diegomtz126@gmail.com>
2024-04-06 19:42:35 +0200

    test(aged-brie-item): aged brie sell in day decreases with each day
    (cherry picked from commit 195b4e1725b0026c50102229ef28bff771bd31fc)

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -53,3 +53,11 @@ class TestGildedRose:
         gilded_rose.update_quality()
 
         assert items[0].quality == 50
+
+    def test_aged_brie_sell_in_day_decreases_every_day(self):
+        items = [Item("Aged Brie", 10, 10)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert items[0].sell_in == 9
