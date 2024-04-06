15a11c9c268c2c32971989c0506aebcb3258ac66
dimanu-py <diegomtz126@gmail.com>
2024-04-06 21:32:45 +0200

    refactor(gilded-rose): substitute logic of backstage passes items for new abstraction

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -1,5 +1,5 @@
 from gilded_rose.src.gilded_rose import Item, GildedRose
-from gilded_rose.src.items import NormalItem, AgedBrie
+from gilded_rose.src.items import NormalItem, AgedBrie, BackstagePasses
 
 BACKSTAGE_PASSES = "Backstage passes"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
@@ -90,7 +90,7 @@ class TestGildedRose:
         assert items[0].sell_in == 10
 
     def test_backstage_passes_quality_increases_by_1_when_sell_in_date_is_more_than_10(self):
-        items = [Item(BACKSTAGE_PASSES, 11, 10)]
+        items = [BackstagePasses(BACKSTAGE_PASSES, 11, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -98,7 +98,7 @@ class TestGildedRose:
         assert items[0].quality == 11
 
     def test_backstage_passes_quality_increases_by_2_when_sell_in_date_is_10_or_less(self):
-        items = [Item(BACKSTAGE_PASSES, 10, 10)]
+        items = [BackstagePasses(BACKSTAGE_PASSES, 10, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -106,7 +106,7 @@ class TestGildedRose:
         assert items[0].quality == 12
 
     def test_backstage_passes_quality_increases_by_3_when_sell_in_date_is_5_or_less(self):
-        items = [Item(BACKSTAGE_PASSES, 5, 10)]
+        items = [BackstagePasses(BACKSTAGE_PASSES, 5, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -114,7 +114,7 @@ class TestGildedRose:
         assert items[0].quality == 13
 
     def test_backstage_passes_quality_drops_to_0_after_sell_in_date(self):
-        items = [Item(BACKSTAGE_PASSES, 0, 10)]
+        items = [BackstagePasses(BACKSTAGE_PASSES, 0, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -122,7 +122,7 @@ class TestGildedRose:
         assert items[0].quality == 0
 
     def test_backstage_passes_sell_in_day_decreases_every_day(self):
-        items = [Item(BACKSTAGE_PASSES, 10, 10)]
+        items = [BackstagePasses(BACKSTAGE_PASSES, 10, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
