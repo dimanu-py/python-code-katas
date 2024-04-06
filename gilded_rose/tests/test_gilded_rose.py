a2cec012c91fb55c0d3cb3afb06d012302b63a27
dimanu-py <diegomtz126@gmail.com>
2024-04-06 19:33:47 +0200

    test(normal-item): sell-in day of a normal item decreases after day has passed
    (cherry picked from commit b0952c2ffbfccf31fc28da8aecd6696e60776f22)

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -11,3 +11,11 @@ class TestGildedRose:
         gilded_rose.update_quality()
 
         assert items[0].quality == 9
+
+    def test_normal_item_decreases_sell_in_every_day(self):
+        items = [Item("foo", 10, 10)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert items[0].sell_in == 9
