1b6b8ffc87db716f1f59c4db1dcbfe9d680fc856
dimanu-py <diegomtz126@gmail.com>
2024-04-06 19:50:00 +0200

    test(backstage-passes-item): backstage passes item increases it quality twice as fast when sell in day is 10 or less
    (cherry picked from commit d4414222a035060cf707344ba11ceee17155790f)

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -96,3 +96,11 @@ class TestGildedRose:
         gilded_rose.update_quality()
 
         assert items[0].quality == 11
+
+    def test_backstage_passes_quality_increases_by_2_when_sell_in_date_is_10_or_less(self):
+        items = [Item(BACKSTAGE_PASSES, 10, 10)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert items[0].quality == 12
