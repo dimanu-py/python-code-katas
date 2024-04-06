5e4e41890980e1436d8eeda4f735a6f966508441
dimanu-py <diegomtz126@gmail.com>
2024-04-06 19:39:50 +0200

    refactor(normal-item): extract foo name to constant
    (cherry picked from commit f329c4ba232d02ec3c067ffeb8238fa0aa832db3)

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -1,11 +1,13 @@
 
 from gilded_rose.src.gilded_rose import Item, GildedRose
 
+NORMAL_ITEM = "foo"
+
 
 class TestGildedRose:
 
     def test_normal_item_decreases_quality_every_day(self):
-        items = [Item("foo", 10, 10)]
+        items = [Item(NORMAL_ITEM, 10, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -13,7 +15,7 @@ class TestGildedRose:
         assert items[0].quality == 9
 
     def test_normal_item_decreases_sell_in_every_day(self):
-        items = [Item("foo", 10, 10)]
+        items = [Item(NORMAL_ITEM, 10, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -21,7 +23,7 @@ class TestGildedRose:
         assert items[0].sell_in == 9
 
     def test_normal_item_decreases_quality_twice_as_fast_after_sell_in_date(self):
-        items = [Item("foo", 0, 10)]
+        items = [Item(NORMAL_ITEM, 0, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -29,7 +31,7 @@ class TestGildedRose:
         assert items[0].quality == 8
 
     def test_quality_of_an_item_is_never_negative(self):
-        items = [Item("foo", 10, 0)]
+        items = [Item(NORMAL_ITEM, 10, 0)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
