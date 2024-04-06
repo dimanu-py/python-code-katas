6333e0b28b8296e32d90117c28add79cb01ac8f7
dimanu-py <diegomtz126@gmail.com>
2024-04-06 19:32:04 +0200

    test(normal-item): quality of a normal item decreases after day has passes
    (cherry picked from commit 8c778f41b6526be7af3c158724ca91d825949f59)

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -4,8 +4,10 @@ from gilded_rose.src.gilded_rose import Item, GildedRose
 
 class TestGildedRose:
 
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_normal_item_decreases_quality_every_day(self):
+        items = [Item("foo", 10, 10)]
         gilded_rose = GildedRose(items)
+
         gilded_rose.update_quality()
-        assert "fixme" == items[0].name
+
+        assert items[0].quality == 9
