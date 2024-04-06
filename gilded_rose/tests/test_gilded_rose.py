00be71facbd3458c8ba6b14c39cb0099fe58c1fe
dimanu-py <diegomtz126@gmail.com>
2024-04-06 21:24:03 +0200

    refactor(gilded-rose): substitute logic of normal items for new abstraction

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -1,5 +1,6 @@
-
 from gilded_rose.src.gilded_rose import Item, GildedRose
+from gilded_rose.src.items import NormalItem
+
 
 BACKSTAGE_PASSES = "Backstage passes"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
@@ -10,7 +11,7 @@ NORMAL_ITEM = "foo"
 class TestGildedRose:
 
     def test_normal_item_decreases_quality_every_day(self):
-        items = [Item(NORMAL_ITEM, 10, 10)]
+        items = [NormalItem(NORMAL_ITEM, 10, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -18,7 +19,7 @@ class TestGildedRose:
         assert items[0].quality == 9
 
     def test_normal_item_decreases_sell_in_every_day(self):
-        items = [Item(NORMAL_ITEM, 10, 10)]
+        items = [NormalItem(NORMAL_ITEM, 10, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -26,7 +27,7 @@ class TestGildedRose:
         assert items[0].sell_in == 9
 
     def test_normal_item_decreases_quality_twice_as_fast_after_sell_in_date(self):
-        items = [Item(NORMAL_ITEM, 0, 10)]
+        items = [NormalItem(NORMAL_ITEM, 0, 10)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
@@ -34,7 +35,7 @@ class TestGildedRose:
         assert items[0].quality == 8
 
     def test_quality_of_an_item_is_never_negative(self):
-        items = [Item(NORMAL_ITEM, 10, 0)]
+        items = [NormalItem(NORMAL_ITEM, 10, 0)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
