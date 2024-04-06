8bb5ce8c1fa6a51ab98a60d6f5f7219889a208f7
dimanu-py <diegomtz126@gmail.com>
2024-04-06 21:39:46 +0200

    refactor(gilded-rose): remove all code structure and use new abstractions using polymorphism
    We've applied Parallel change strategy, including step by step our new functionalities and keeping the old code until we've migrated all the behaviors

--- gilded_rose/src/gilded_rose.py
+++ gilded_rose/src/gilded_rose.py
@@ -1,38 +1,11 @@
-from gilded_rose.src.items import GildedRoseItem, Item
-
-BACKSTAGE_PASSES = "Backstage passes"
-AGED_BRIE = "Aged Brie"
-SULFURA = "Sulfuras, Hand of Ragnaros"
-
-MAXIMUM_QUALITY = 50
-MINIMUM_QUALITY = 0
+from gilded_rose.src.items import GildedRoseItem
 
 
-class GildedRose(object):
+class GildedRose:
 
-    def __init__(self, items: list[Item]) -> None:
+    def __init__(self, items: list[GildedRoseItem]) -> None:
         self.items = items
 
     def update_quality(self) -> None:
         for item in self.items:
-            self.process_item(item)
-
-    def process_item(self, item: Item | GildedRoseItem) -> None:
-
-        if item.name == SULFURA:
             item.process_item()
-
-        if item.name == AGED_BRIE:
-            item.process_item()
-        elif item.name == BACKSTAGE_PASSES:
-            item.process_item()
-        else:
-            item.process_item()
-
-    def increase_quality(self, item: Item) -> None:
-        if item.quality < MAXIMUM_QUALITY:
-            item.quality = item.quality + 1
-
-    def decrease_quality(self, item: Item) -> None:
-        if item.quality > MINIMUM_QUALITY:
-            item.quality = item.quality - 1
