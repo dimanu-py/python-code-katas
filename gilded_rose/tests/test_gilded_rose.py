d9de812fb31e6d1fbb277beb8f34e673db913d6e
dimanu-py <diegomtz126@gmail.com>
2024-04-06 19:40:48 +0200

    test(aged-brie-item): aged brie quality increases the older it gets
    (cherry picked from commit 22b7c623083768ef771c35b2a26dc017354e2fdc)

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -37,3 +37,11 @@ class TestGildedRose:
         gilded_rose.update_quality()
 
         assert items[0].quality == 0
+
+    def test_aged_brie_increases_quality_every_day(self):
+        items = [Item("Aged Brie", 10, 10)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert items[0].quality == 11
