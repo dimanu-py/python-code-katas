943e3b441a3a1ed54f8e9aaf3c6242111dc14add
dimanu-py <diegomtz126@gmail.com>
2024-04-20 14:24:25 +0200

    refactor(expand): add new price item to list of prices
    (cherry picked from commit d004e0b37bc407cc5353917af266330d93171492)

--- parallel_change/src/field.py
+++ parallel_change/src/field.py
@@ -14,6 +14,7 @@ class ShoppingCart:
 
         self.price += price
         self.number_products += 1
+        self.prices.append(price)
 
     def calculate_total_price(self) -> float:
         return self.price
