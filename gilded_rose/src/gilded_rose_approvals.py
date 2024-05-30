74ffe2de95aafd520614e417aa4a44f3b356f8b5
dimanu-py <diegomtz126@gmail.com>
2024-05-30 20:04:54 +0200

    refactor(gilded-rose): update sell in value at the beginning of the update quality process
    (cherry picked from commit c7fcee5585f5449f874e25634de46f832aeeb282)

--- gilded_rose/src/gilded_rose_approvals.py
+++ gilded_rose/src/gilded_rose_approvals.py
@@ -29,20 +29,21 @@ class GildedRose:
         if item.name == SULFURAS:
             return
 
+        item.sell_in = item.sell_in - QUALITY_STEP
+
         if item.name == AGED_BRIE or item.name == BACKSTAGE_PASSES:
             if item.quality < MAX_QUALITY:
                 item.quality = item.quality + QUALITY_STEP
                 if item.name == BACKSTAGE_PASSES:
-                    if item.sell_in < 11:
+                    if item.sell_in < 10:
                         if item.quality < MAX_QUALITY:
                             item.quality = item.quality + QUALITY_STEP
-                    if item.sell_in < 6:
+                    if item.sell_in < 5:
                         if item.quality < MAX_QUALITY:
                             item.quality = item.quality + QUALITY_STEP
         else:
             if item.quality > MIN_QUALITY:
                 item.quality = item.quality - QUALITY_STEP
-        item.sell_in = item.sell_in - QUALITY_STEP
 
         if item.sell_in < 0:
             if item.name == AGED_BRIE:
