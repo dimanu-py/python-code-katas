e7b3011178236d1836cf5e674ad3b02db14b96e7
dimanu-py <diegomtz126@gmail.com>
2024-05-31 15:07:45 +0200

    refactor(gilded-rose): move Item class to its own file

---
+++ gilded_rose/src/items_approvals.py
@@ -0,0 +1,13 @@
+
+MIN_QUALITY = 0
+MAX_QUALITY = 50
+QUALITY_STEP = 1
+
+class Item:
+    def __init__(self, name: str, sell_in: int, quality: int) -> None:
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
