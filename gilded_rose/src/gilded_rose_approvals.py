a36325873470c2d97f01e7ce55dc38184d75432b
dimanu-py <diegomtz126@gmail.com>
2024-05-31 18:28:46 +0200

    refactor(gilded-rose): remove dead code and leave call to items behavior using polymorphism
    (cherry picked from commit 1b4e569af12372c39900c0b2998d562c199ef3cb)

--- gilded_rose/src/gilded_rose_approvals.py
+++ gilded_rose/src/gilded_rose_approvals.py
@@ -1,34 +1,11 @@
-from gilded_rose_approvals.items import Item, CommonItem, AgedBrieItem, BackstagePassesItem, SulfurasItem
-
-QUALITY_STEP = 1
-MIN_QUALITY = 0
-MAX_QUALITY = 50
-SULFURAS = "Sulfuras, Hand of Ragnaros"
-BACKSTAGE_PASSES = "Backstage passes"
-AGED_BRIE = "Aged Brie"
+from gilded_rose_approvals.items import GildedRoseItem
 
 
 class GildedRose:
 
-    def __init__(self, items: list[Item]) -> None:
+    def __init__(self, items: list[GildedRoseItem]) -> None:
         self.items = items
 
     def process_items(self) -> None:
         for item in self.items:
-            self.update_quality(item)
-
-    def update_quality(self, item: Item) -> None:
-        if isinstance(item, SulfurasItem):
-            return
-
-        self.decrease_sell_in(item)
-
-        if isinstance(item, CommonItem):
-            item.update_quality()
-        elif isinstance(item, AgedBrieItem):
-            item.update_quality()
-        elif isinstance(item, BackstagePassesItem):
-            item.update_quality()
-
-    def decrease_sell_in(self, item: Item) -> None:
-        item.sell_in = item.sell_in - QUALITY_STEP
+            item.process_item()
