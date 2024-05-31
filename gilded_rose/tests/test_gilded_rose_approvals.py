520668271695e0b474296621a13ddaf0bfa41d67
dimanu-py <diegomtz126@gmail.com>
2024-05-31 18:30:53 +0200

    refactor(test-gilded-rose): extract strings names to constants

--- gilded_rose/tests/test_gilded_rose_approvals.py
+++ gilded_rose/tests/test_gilded_rose_approvals.py
@@ -3,6 +3,11 @@ from approvaltests import verify_all_combinations
 from gilded_rose.src.gilded_rose_approvals import GildedRose
 from gilded_rose.src.items_approvals import Item, CommonItem, AgedBrieItem, BackstagePassesItem, SulfurasItem
 
+COMMON_ITEM = "Common Item"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+BACKSTAGE_PASSES = "Backstage passes"
+AGED_BRIE = "Aged Brie"
+
 
 def item_printer(item: Item) -> str:
     return f"{item.name}, sell_in: {item.sell_in}, quality: {item.quality}"
@@ -11,7 +16,7 @@ def item_printer(item: Item) -> str:
 class TestGildedRoseApprovals:
 
     def test_update_quality(self, pycharm_diff_reporter):
-        name = ["Common Item", "Aged Brie", "Backstage passes", "Sulfuras, Hand of Ragnaros"]
+        name = [COMMON_ITEM, AGED_BRIE, BACKSTAGE_PASSES, SULFURAS]
         sell_in = [-1, 0, 5, 6, 10, 11]
         quality = [-1, 0, 1, 2, 49, 50]
 
@@ -32,10 +37,10 @@ class TestGildedRoseApprovals:
         return item_as_string
 
     def select_item(self, name: str, sell_in: int, quality: int) -> GildedRoseItem:
-        if name == "Aged Brie":
+        if name == AGED_BRIE:
             return AgedBrieItem(name, sell_in, quality)
-        elif name == "Backstage passes":
+        elif name == BACKSTAGE_PASSES:
             return BackstagePassesItem(name, sell_in, quality)
-        elif name == "Sulfuras, Hand of Ragnaros":
+        elif name == SULFURAS:
             return SulfurasItem(name, sell_in, quality)
         return CommonItem(name, sell_in, quality)
