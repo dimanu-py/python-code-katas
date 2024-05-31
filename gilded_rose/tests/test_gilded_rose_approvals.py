3e8ce9dd5e48cedf7dc7e3e9d29601dcd23ab5b4
dimanu-py <diegomtz126@gmail.com>
2024-05-31 18:43:16 +0200

    feat(items): add new ConjuredItem
    This item decreases its quality twice as fast than a CommonItem

--- gilded_rose/tests/test_gilded_rose_approvals.py
+++ gilded_rose/tests/test_gilded_rose_approvals.py
@@ -1,12 +1,13 @@
 from approvaltests import verify_all_combinations
 
 from gilded_rose.src.gilded_rose_approvals import GildedRose
-from gilded_rose.src.items_approvals import Item, CommonItem, AgedBrieItem, BackstagePassesItem, SulfurasItem
+from gilded_rose.src.items_approvals import Item, CommonItem, AgedBrieItem, BackstagePassesItem, SulfurasItem, ConjuredItem
 
 COMMON_ITEM = "Common Item"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
 BACKSTAGE_PASSES = "Backstage passes"
 AGED_BRIE = "Aged Brie"
+CONJURED = "Conjured"
 
 
 def item_printer(item: Item) -> str:
@@ -16,7 +17,7 @@ def item_printer(item: Item) -> str:
 class TestGildedRoseApprovals:
 
     def test_update_quality(self, pycharm_diff_reporter):
-        name = [COMMON_ITEM, AGED_BRIE, BACKSTAGE_PASSES, SULFURAS]
+        name = [COMMON_ITEM, AGED_BRIE, BACKSTAGE_PASSES, SULFURAS, CONJURED]
         sell_in = [-1, 0, 5, 6, 10, 11]
         quality = [-1, 0, 1, 2, 49, 50]
 
@@ -43,4 +44,6 @@ class TestGildedRoseApprovals:
             return BackstagePassesItem(name, sell_in, quality)
         elif name == SULFURAS:
             return SulfurasItem(name, sell_in, quality)
+        elif name == CONJURED:
+            return ConjuredItem(name, sell_in, quality)
         return CommonItem(name, sell_in, quality)
