9bec33fe68b987dbe0ce487fa8352ed75caad5ad
dimanu-py <diegomtz126@gmail.com>
2024-06-23 20:36:58 +0200

    feat(trip-service): create project structure and set instructions for Trip Service kata

---
+++ trip_service/src/trip.py
@@ -0,0 +1,12 @@
+from trip_service.src.exceptions import DependendClassCallDuringUnitTestException
+from trip_service.src.user import User
+
+
+class Trip:
+  pass
+
+
+class TripRepository:
+  @staticmethod
+  def find_trips_by_user(user: User) -> Trip:
+    raise DependendClassCallDuringUnitTestException("TripDAO should not be invoked on an unit test.")
