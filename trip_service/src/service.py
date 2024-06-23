59c80f3b584f8d746313ef522e5bc534fe499d9a
dimanu-py <diegomtz126@gmail.com>
2024-06-23 20:36:58 +0200

    feat(trip-service): create project structure and set instructions for Trip Service kata

---
+++ trip_service/src/service.py
@@ -0,0 +1,22 @@
+from trip_service.src.exceptions import UserNotLoggedInException
+from trip_service.src.trip import Trip, TripRepository
+from trip_service.src.user import User
+from trip_service.src.user_session import UserSession
+
+
+class TripService:
+
+    def get_trips_by_user(self, user: User) -> list[Trip]:
+        logged_user = UserSession.get_instance().get_logged_user()
+        is_friend = False
+        trip_list = []
+        if logged_user:
+          for friend in user.get_friends():
+            if friend is logged_user:
+              is_friend = True
+              break
+          if is_friend:
+            trip_list = TripRepository.find_trips_by_user(user)
+          return trip_list
+        else:
+            raise UserNotLoggedInException()
---
+++ trip_service/src/trip.py
@@ -0,0 +1,13 @@
+from trip_service.src.exceptions import DependendClassCallDuringUnitTestException
+from trip_service.src.user import User
+
+
+class Trip:
+  pass
+
+
+class TripRepository:
+
+  @staticmethod
+  def find_trips_by_user(user: User) -> list[Trip]:
+    raise DependendClassCallDuringUnitTestException("TripDAO should not be invoked on an unit test.")
