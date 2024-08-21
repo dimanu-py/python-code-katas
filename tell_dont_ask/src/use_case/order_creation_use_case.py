77c80453d9b6495cbd0b63a551b355185d524b93
dimanu-py <diegomtz126@gmail.com>
2024-08-21 03:20:06 +0200

    refactor(order-creation-use-case): turn order into a field with default values

--- tell_dont_ask/src/use_case/order_creation_use_case.py
+++ tell_dont_ask/src/use_case/order_creation_use_case.py
@@ -1,6 +1,5 @@
 from tell_dont_ask.src.domain.order import Order
 from tell_dont_ask.src.domain.order_item import OrderItem
-from tell_dont_ask.src.domain.order_status import OrderStatus
 from tell_dont_ask.src.repository.order_repository import OrderRepository
 from tell_dont_ask.src.repository.product_catalog import ProductCatalog
 from tell_dont_ask.src.use_case.exceptions import UnknownProductException
@@ -11,16 +10,9 @@ class OrderCreationUseCase:
     def __init__(self, order_repository: OrderRepository, product_catalog: ProductCatalog):
         self._order_repository = order_repository
         self._product_catalog = product_catalog
+        self.order = Order()
 
     def run(self, request: SellItemsRequest) -> None:
-        order = Order(
-            status=OrderStatus.CREATED,
-            items=[],
-            currency='EUR',
-            total=0,
-            tax=0
-        )
-
         for item_request in request.products:
             product = self._product_catalog.get_by_name(item_request.product_name)
 
@@ -36,9 +28,9 @@ class OrderCreationUseCase:
                 tax=tax,
                 taxed_amount=product_price_including_tax
             )
-            order.items.append(order_item)
+            self.order.items.append(order_item)
 
-            order.total += product_price_including_tax
-            order.tax += tax
+            self.order.total += product_price_including_tax
+            self.order.tax += tax
 
-        self._order_repository.save(order)
+        self._order_repository.save(self.order)
