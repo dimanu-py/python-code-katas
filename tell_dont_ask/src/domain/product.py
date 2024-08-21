f382570160dd133f67388c13dd1f8cb9644b6501
dimanu-py <diegomtz126@gmail.com>
2024-08-21 03:08:33 +0200

    refactor(product): convert calculate_unitary_taxed_amount method to protected
    (cherry picked from commit 508e9681ae4831e3bf32758ba89b9dae3749d6bb)

--- tell_dont_ask/src/domain/product.py
+++ tell_dont_ask/src/domain/product.py
@@ -12,8 +12,8 @@ class Product:
     def calculate_unitary_tax(self) -> float:
         return round(self.price / 100 * self.category.tax_percentage, 2)
 
-    def calculate_unitary_taxed_amount(self) -> float:
+    def _calculate_unitary_taxed_amount(self) -> float:
         return round(self.price + self.calculate_unitary_tax(), 2)
 
     def calculated_taxed_amount(self, quantity: int) -> float:
-        return round(self.calculate_unitary_taxed_amount() * quantity, 2)
+        return round(self._calculate_unitary_taxed_amount() * quantity, 2)
