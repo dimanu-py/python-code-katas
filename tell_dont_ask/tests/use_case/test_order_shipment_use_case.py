d91b935b46fe0fcca57dfe4cbe67b5523292c4fd
dimanu-py <diegomtz126@gmail.com>
2024-08-21 04:07:40 +0200

    refactor(test-order-shipment): use order fixtures

--- tell_dont_ask/tests/use_case/test_order_shipment_use_case.py
+++ tell_dont_ask/tests/use_case/test_order_shipment_use_case.py
@@ -1,10 +1,9 @@
 import pytest
 
-from tell_dont_ask.src.domain import Order
-from tell_dont_ask.src.domain import OrderStatus
-from tell_dont_ask.src import OrderCannotBeShippedException, OrderCannotBeShippedTwiceException
+from tell_dont_ask.src.domain.order_status import OrderStatus
+from tell_dont_ask.src.use_case.exceptions import OrderCannotBeShippedException, OrderCannotBeShippedTwiceException
 from tell_dont_ask.src.use_case.order_shipment_request import OrderShipmentRequest
-from tell_dont_ask.src import OrderShipmentUseCase
+from tell_dont_ask.src.use_case.order_shipment_use_case import OrderShipmentUseCase
 from tell_dont_ask.tests.doubles.stub_order_repository import StubOrderRepository
 from tell_dont_ask.tests.doubles.stub_shipment_service import StubShipmentService
 
@@ -15,34 +14,20 @@ class TestOrderShipmentUseCase:
         self.shipment_service = StubShipmentService()
         self.use_case = OrderShipmentUseCase(self.order_repository, self.shipment_service)
 
-    def test_ship_approved_order(self):
-        initial_order = Order(id=1, status=OrderStatus.APPROVED)
-        self.order_repository.add_order(initial_order)
+    def test_ship_approved_order(self, approved_order):
+        self.order_repository.add_order(approved_order)
 
         request = OrderShipmentRequest(order_id=1)
 
         self.use_case.run(request)
 
         assert self.order_repository.inserted_order.status == OrderStatus.SHIPPED
-        assert self.shipment_service.shipped_order == initial_order
+        assert self.shipment_service.shipped_order == approved_order
 
-    def test_created_orders_cannot_be_shipped(self):
-        initial_order = Order(id=2, status=OrderStatus.CREATED)
-        self.order_repository.add_order(initial_order)
+    def test_created_orders_cannot_be_shipped(self, created_order):
+        self.order_repository.add_order(created_order)
 
-        request = OrderShipmentRequest(order_id=2)
-
-        with pytest.raises(OrderCannotBeShippedException):
-            self.use_case.run(request)
-
-        assert self.order_repository.inserted_order is None
-        assert self.shipment_service.shipped_order is None
-
-    def test_rejected_orders_cannot_be_shipped(self):
-        initial_order = Order(id=3, status=OrderStatus.REJECTED)
-        self.order_repository.add_order(initial_order)
-
-        request = OrderShipmentRequest(order_id=3)
+        request = OrderShipmentRequest(order_id=1)
 
         with pytest.raises(OrderCannotBeShippedException):
             self.use_case.run(request)
@@ -50,11 +35,21 @@ class TestOrderShipmentUseCase:
         assert self.order_repository.inserted_order is None
         assert self.shipment_service.shipped_order is None
 
-    def test_shipped_orders_cannot_be_shipped_again(self):
-        initial_order = Order(id=4, status=OrderStatus.SHIPPED)
-        self.order_repository.add_order(initial_order)
+    def test_rejected_orders_cannot_be_shipped(self, rejected_order):
+        self.order_repository.add_order(rejected_order)
 
-        request = OrderShipmentRequest(order_id=4)
+        request = OrderShipmentRequest(order_id=1)
+
+        with pytest.raises(OrderCannotBeShippedException):
+            self.use_case.run(request)
+
+        assert self.order_repository.inserted_order is None
+        assert self.shipment_service.shipped_order is None
+
+    def test_shipped_orders_cannot_be_shipped_again(self, shipped_order):
+        self.order_repository.add_order(shipped_order)
+
+        request = OrderShipmentRequest(order_id=1)
 
         with pytest.raises(OrderCannotBeShippedTwiceException):
             self.use_case.run(request)
