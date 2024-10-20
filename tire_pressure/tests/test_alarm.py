a07713f35299ab767894080efd4131493f02b9fd
dimanu-py <diegomtz126@gmail.com>
2024-10-20 16:50:44 +0200

    test(alarm): verify alarm is on when pressure is below threshold

--- tire_pressure/tests/test_alarm.py
+++ tire_pressure/tests/test_alarm.py
@@ -1,4 +1,6 @@
-from expects import expect, be_false
+from unittest.mock import patch
+
+from expects import expect, be_false, be_true
 
 from tire_pressure.src.alarm import Alarm
 
@@ -9,3 +11,12 @@ class TestAlarm:
         alarm = Alarm()
 
         expect(alarm.is_alarm_on).to(be_false)
+
+    def test_alarm_is_on_when_pressure_is_below_threshold(self):
+        alarm = Alarm()
+
+        with patch("tire_pressure.src.sensor.Sensor.pop_next_pressure_psi_value") as pressure:
+            pressure.return_value = 16
+            alarm.check_pressure()
+
+            expect(alarm.is_alarm_on).to(be_true)
