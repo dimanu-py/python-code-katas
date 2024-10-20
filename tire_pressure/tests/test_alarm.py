8fd0b2d19c1c0f6e0430b50806993d4d2098af5c
dimanu-py <diegomtz126@gmail.com>
2024-10-20 17:03:28 +0200

    refactor(test-alarm): extract pressure values to give domain language to tests

--- tire_pressure/tests/test_alarm.py
+++ tire_pressure/tests/test_alarm.py
@@ -5,6 +5,10 @@ from expects import expect, be_false, be_true
 
 from tire_pressure.src.alarm import Alarm
 
+CORRECT_PRESSURE = 20
+HIGH_PRESSURE = 22
+LOW_PRESSURE = 16
+
 
 class TestAlarm:
 
@@ -14,7 +18,7 @@ class TestAlarm:
     def test_alarm_is_off_by_default(self):
         expect(self.alarm.is_alarm_on).to(be_false)
 
-    @pytest.mark.parametrize("pressure_read", [16, 22])
+    @pytest.mark.parametrize("pressure_read", [LOW_PRESSURE, HIGH_PRESSURE])
     def test_alarm_is_on_when_pressure_is_not_between_thresholds(self, pressure_read):
         with patch("tire_pressure.src.sensor.Sensor.pop_next_pressure_psi_value") as pressure:
             pressure.return_value = pressure_read
@@ -24,7 +28,7 @@ class TestAlarm:
 
     def test_alarm_is_off_when_pressure_is_in_threshold(self):
         with patch("tire_pressure.src.sensor.Sensor.pop_next_pressure_psi_value") as preesure:
-            preesure.return_value = 20
+            preesure.return_value = CORRECT_PRESSURE
             self.alarm.check_pressure()
 
             expect(self.alarm.is_alarm_on).to(be_false)
