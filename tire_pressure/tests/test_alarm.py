aeab491c25ee8a852b1a9665e39c27e7043d63f1
dimanu-py <diegomtz126@gmail.com>
2024-10-20 16:59:35 +0200

    refactor(test-alarm): remove test with duplicated structure into a single parametrized test

--- tire_pressure/tests/test_alarm.py
+++ tire_pressure/tests/test_alarm.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 
+import pytest
 from expects import expect, be_false, be_true
 
 from tire_pressure.src.alarm import Alarm
@@ -13,16 +14,10 @@ class TestAlarm:
     def test_alarm_is_off_by_default(self):
         expect(self.alarm.is_alarm_on).to(be_false)
 
-    def test_alarm_is_on_when_pressure_is_below_threshold(self):
+    @pytest.mark.parametrize("pressure_read", [16, 22])
+    def test_alarm_is_on_when_pressure_is_not_between_thresholds(self, pressure_read):
         with patch("tire_pressure.src.sensor.Sensor.pop_next_pressure_psi_value") as pressure:
-            pressure.return_value = 16
-            self.alarm.check_pressure()
-
-            expect(self.alarm.is_alarm_on).to(be_true)
-
-    def test_alarm_is_on_when_pressure_is_above_threshold(self):
-        with patch("tire_pressure.src.sensor.Sensor.pop_next_pressure_psi_value") as pressure:
-            pressure.return_value = 22
+            pressure.return_value = pressure_read
             self.alarm.check_pressure()
 
             expect(self.alarm.is_alarm_on).to(be_true)
