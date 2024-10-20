b5348acf09e944653f1b02c3110ebf00d51175c2
dimanu-py <diegomtz126@gmail.com>
2024-10-20 16:55:28 +0200

    refactor(test-alarm): extract alarm instantiation to setup method for cleaner test

--- tire_pressure/tests/test_alarm.py
+++ tire_pressure/tests/test_alarm.py
@@ -7,25 +7,22 @@ from tire_pressure.src.alarm import Alarm
 
 class TestAlarm:
 
-    def test_alarm_is_off_by_default(self):
-        alarm = Alarm()
+    def setup_method(self):
+        self.alarm = Alarm()
 
-        expect(alarm.is_alarm_on).to(be_false)
+    def test_alarm_is_off_by_default(self):
+        expect(self.alarm.is_alarm_on).to(be_false)
 
     def test_alarm_is_on_when_pressure_is_below_threshold(self):
-        alarm = Alarm()
-
         with patch("tire_pressure.src.sensor.Sensor.pop_next_pressure_psi_value") as pressure:
             pressure.return_value = 16
-            alarm.check_pressure()
+            self.alarm.check_pressure()
 
-            expect(alarm.is_alarm_on).to(be_true)
+            expect(self.alarm.is_alarm_on).to(be_true)
 
     def test_alarm_is_on_when_pressure_is_above_threshold(self):
-        alarm = Alarm()
-
         with patch("tire_pressure.src.sensor.Sensor.pop_next_pressure_psi_value") as pressure:
             pressure.return_value = 22
-            alarm.check_pressure()
+            self.alarm.check_pressure()
 
-            expect(alarm.is_alarm_on).to(be_true)
+            expect(self.alarm.is_alarm_on).to(be_true)
