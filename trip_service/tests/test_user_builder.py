fb24af7f844153cdfdaa89677ac48ed771e12817
dimanu-py <diegomtz126@gmail.com>
2024-10-21 21:50:10 +0200

    feat(user-builder): build user that has friends

--- trip_service/tests/test_user_builder.py
+++ trip_service/tests/test_user_builder.py
@@ -1,9 +1,12 @@
-from expects import be_empty, expect
+from expects import be_empty, expect, equal
 
+from trip_service.src.user import User
 from trip_service.src.user_builder import UserBuilder
 
 
 class TestUserBuilder:
+
+    ANY_USER = User()
 
     def test_user_has_no_friends_nor_trips_initially(self):
         builder = UserBuilder()
@@ -12,3 +15,11 @@ class TestUserBuilder:
 
         expect(user.friends).to(be_empty)
         expect(user.trips).to(be_empty)
+
+    def test_user_has_friends(self):
+        builder = UserBuilder()
+
+        user = builder.friend_of(self.ANY_USER).build()
+
+        expect(user.friends).to(equal([self.ANY_USER]))
+    
