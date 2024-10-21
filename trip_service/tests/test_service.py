8387daabd43663fffebea6a83d34570d50796964
dimanu-py <diegomtz126@gmail.com>
2024-10-21 20:37:32 +0200

    feat(test-service): create SeamTripService to bypass the call to UserSession in tests

--- trip_service/tests/test_service.py
+++ trip_service/tests/test_service.py
@@ -1,3 +1,11 @@
+from trip_service.src.service import TripService
+from trip_service.src.user import User
+
+
+class SeamTripService(TripService):
+
+    def get_logged_user(self) -> User:
+        return None
 
 
 class TestTripService:
