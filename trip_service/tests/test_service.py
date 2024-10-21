f7b13f46086cb3ab17495502cfb5b7caf7a7d055
dimanu-py <diegomtz126@gmail.com>
2024-10-21 20:58:52 +0200

    test(service): verify that two users need to be friends to get each others trips
    When creating the stranger we don't really need to add a friend, it's just to be more explicit that the logged user and the stranger are not friends

--- trip_service/tests/test_service.py
+++ trip_service/tests/test_service.py
@@ -1,4 +1,4 @@
-from expects import expect, raise_error
+from expects import expect, raise_error, be_empty
 
 from trip_service.src.exceptions import UserNotLoggedInException
 from trip_service.src.service import TripService
@@ -17,8 +17,19 @@ class SeamTripService(TripService):
 class TestTripService:
 
     GUEST_USER = None
+    LOGGED_USER = User()
+    GENERIC_USER = User()
 
     def test_user_needs_to_be_logged_in(self):
         trip_service = SeamTripService(self.GUEST_USER)
 
         expect(lambda: trip_service.get_trips_by_user(self.GUEST_USER)).to(raise_error(UserNotLoggedInException))
+
+    def test_logged_user_gets_no_trips_if_is_not_friend(self):
+        trip_service = SeamTripService(self.LOGGED_USER)
+        stranger = User()
+        stranger.add_friend(self.GENERIC_USER)
+
+        trips = trip_service.get_trips_by_user(stranger)
+
+        expect(trips).to(be_empty)
