035bbceb43890825534a114f9d71ecdfbf0a9a31
dimanu-py <diegomtz126@gmail.com>
2024-10-21 22:41:53 +0200

    refactor(service): inject the logged user to avoid depending on the external service UserSession

--- trip_service/tests/test_service.py
+++ trip_service/tests/test_service.py
@@ -9,12 +9,6 @@ from trip_service.src.user_builder import UserBuilder
 
 class SeamTripService(TripService):
 
-    def __init__(self, user: User | None) -> None:
-        self.user = user
-
-    def get_logged_user(self) -> User:
-        return self.user
-
     def get_trips_from(self, user: User) -> list[Trip]:
         return user.get_trips()
 
@@ -27,25 +21,25 @@ class TestTripService:
     CANADA_TRIP = Trip()
 
     def test_user_needs_to_be_logged_in(self):
-        trip_service = SeamTripService(self.GUEST_USER)
+        trip_service = SeamTripService()
 
-        expect(lambda: trip_service.get_trips_by_user(self.GENERIC_USER)).to(raise_error(UserNotLoggedInException))
+        expect(lambda: trip_service.get_trips_by_user(self.GUEST_USER, self.GENERIC_USER)).to(raise_error(UserNotLoggedInException))
 
     def test_logged_user_gets_no_trips_if_is_not_friend(self):
-        trip_service = SeamTripService(self.LOGGED_USER)
+        trip_service = SeamTripService()
         stranger = UserBuilder().friend_of(self.GENERIC_USER).build()
 
-        trips = trip_service.get_trips_by_user(stranger)
+        trips = trip_service.get_trips_by_user(self.LOGGED_USER, stranger)
 
         expect(trips).to(be_empty)
 
     def test_logged_user_gets_friend_trips(self):
-        trip_service = SeamTripService(self.LOGGED_USER)
+        trip_service = SeamTripService()
         friend = (UserBuilder()
                   .friend_of(self.LOGGED_USER, self.GENERIC_USER)
                   .has_travel_to(self.CANADA_TRIP)
                   .build())
 
-        trips = trip_service.get_trips_by_user(friend)
+        trips = trip_service.get_trips_by_user(self.LOGGED_USER, friend)
 
         expect(trips).to(equal([self.CANADA_TRIP]))
