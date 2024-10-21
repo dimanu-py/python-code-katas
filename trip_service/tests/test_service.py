af99d6481060eb31c17a2e1459f01bb881f13a1e
dimanu-py <diegomtz126@gmail.com>
2024-10-21 20:39:24 +0200

    test(service): verify user needs to be logged in to use the application

--- trip_service/tests/test_service.py
+++ trip_service/tests/test_service.py
@@ -1,3 +1,6 @@
+from expects import expect, raise_error
+
+from trip_service.src.exceptions import UserNotLoggedInException
 from trip_service.src.service import TripService
 from trip_service.src.user import User
 
@@ -9,4 +12,10 @@ class SeamTripService(TripService):
 
 
 class TestTripService:
-    pass
+
+    GUEST_USER = None
+
+    def test_user_needs_to_be_logged_in(self):
+        trip_service = SeamTripService()
+
+        expect(lambda: trip_service.get_trips_by_user(self.GUEST_USER)).to(raise_error(UserNotLoggedInException))
