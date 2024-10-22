18cf227ef0d4929bf962cbcd35999996de18d966
dimanu-py <diegomtz126@gmail.com>
2024-10-22 08:23:03 +0200

    refactor(test-service): create field to represent how the domain the express an empty list of trips

--- trip_service/src/service.py
+++ trip_service/src/service.py
@@ -8,11 +8,12 @@ class TripService:
 
     def __init__(self, repository: TripRepository) -> None:
         self.repository = repository
+        self.no_trips = []
 
     def get_trips_by_user(self, logged_user: User, requested_user: User) -> list[Trip]:
         self._verify(logged_user)
 
-        return self.repository.find_trips_of(requested_user) if requested_user.is_friend_with(logged_user) else []
+        return self.repository.find_trips_of(requested_user) if requested_user.is_friend_with(logged_user) else self.no_trips
 
     @staticmethod
     def _verify(logged_user: User) -> None:
