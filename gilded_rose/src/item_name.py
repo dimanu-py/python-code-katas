c35074f26a38e5b4c78a1662ed882ef8efaa8e65
dimanu-py <diegomtz126@gmail.com>
2024-10-24 09:55:29 +0200

    feat(item-name): add methods to ItemName to know which item it is

--- gilded_rose/src/item_name.py
+++ gilded_rose/src/item_name.py
@@ -1,3 +1,7 @@
+CONJURED = "Conjured"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+BACKSTAGE_PASSES = "Backstage passes"
+AGED_BRIE = "Aged Brie"
 
 
 class ItemName:
@@ -7,3 +11,15 @@ class ItemName:
 
     def __str__(self) -> str:
         return self.value
+
+    def is_aged_brie(self) -> bool:
+        return self.value == AGED_BRIE
+
+    def is_backstage_passes(self) -> bool:
+        return self.value == BACKSTAGE_PASSES
+
+    def is_sulfuras(self) -> bool:
+        return self.value == SULFURAS
+
+    def is_conjured(self) -> bool:
+        return self.value == CONJURED
