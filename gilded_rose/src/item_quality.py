60022a1162d6ae86cd175bdd57552109303a4e52
dimanu-py <diegomtz126@gmail.com>
2024-10-24 10:49:06 +0200

    feat(item-quality): add methods to be able to increase, decrease and reset to 0 quality

--- gilded_rose/src/item_quality.py
+++ gilded_rose/src/item_quality.py
@@ -7,6 +7,7 @@ class NegativeQualityValueError(Exception):
 
 class ItemQuality:
     MIN_QUALITY = 0
+    MAX_QUALITY = 50
 
     def __init__(self, quality: int) -> None:
         if quality < self.MIN_QUALITY:
@@ -15,3 +16,12 @@ class ItemQuality:
 
     def __str__(self) -> str:
         return str(self.value)
+
+    def increase(self, amount: int = 1) -> "ItemQuality":
+        return ItemQuality(min(self.value + amount, self.MAX_QUALITY))
+
+    def decrease(self, amount: int = 1) -> "ItemQuality":
+        return ItemQuality(max(self.value - amount, self.MIN_QUALITY))
+
+    def reset(self) -> "ItemQuality":
+        return ItemQuality(self.MIN_QUALITY)
