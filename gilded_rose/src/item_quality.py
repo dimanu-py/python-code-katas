de35ebc7baefe32965cc9d9df40c2ee00d5a8e5c
dimanu-py <diegomtz126@gmail.com>
2024-10-24 10:44:36 +0200

    feat(item-quality): validate that quality is not negative when creating object

--- gilded_rose/src/item_quality.py
+++ gilded_rose/src/item_quality.py
@@ -1,6 +1,16 @@
+class NegativeQualityValueError(Exception):
+    """Negative quality value has been introduced."""
+
+    def __init__(self, quality: int):
+        self.message = f"Quality value cannot be negative. Current value: {quality}"
+        super().__init__(self.message)
+
 class ItemQuality:
+    MIN_QUALITY = 0
 
     def __init__(self, quality: int) -> None:
+        if quality < self.MIN_QUALITY:
+            raise NegativeQualityValueError(quality)
         self.value = quality
 
     def __str__(self) -> str:
