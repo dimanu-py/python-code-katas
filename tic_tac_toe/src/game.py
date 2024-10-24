af6d5cc9d73e6fa8e7bdfe2597138f336c64e2f1
dimanu-py <diegomtz126@gmail.com>
2024-10-24 14:32:49 +0200

    refactor(game): introduce the concept of Tile in the game

--- tic_tac_toe/src/game.py
+++ tic_tac_toe/src/game.py
@@ -1,5 +1,6 @@
 from tic_tac_toe.src.invalid_turn import InvalidTurnError
 from tic_tac_toe.src.player import Player
+from tic_tac_toe.src.tile import Tile
 
 
 class Game:
@@ -7,7 +8,7 @@ class Game:
     def __init__(self) -> None:
         self.player_to_play = Player.O
 
-    def play(self, player: str) -> None:
+    def play(self, player: str, tile: Tile) -> None:
         if player == self.player_to_play:
             raise InvalidTurnError(player)
 
