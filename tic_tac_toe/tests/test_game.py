16bf214daa72c620d4254fc0eb7760d64ab6d062
dimanu-py <diegomtz126@gmail.com>
2024-10-24 15:00:23 +0200

    feat(game): mark tile by player

--- tic_tac_toe/tests/test_game.py
+++ tic_tac_toe/tests/test_game.py
@@ -1,4 +1,4 @@
-from expects import expect, raise_error
+from expects import expect, raise_error, be_empty, contain
 
 from tic_tac_toe.src.game import Game
 from tic_tac_toe.src.invalid_turn import InvalidTurnError
@@ -25,3 +25,10 @@ class TestGame:
         self.game.play(player="X", tile=Tile.TOP_LEFT)
 
         expect(lambda: self.game.play(player="O", tile=Tile.TOP_LEFT)).not_to(raise_error(InvalidTurnError))
+
+    def test_player_can_mark_a_tile(self):
+        tile_to_play = Tile.TOP_LEFT
+        self.game.play(player="X", tile=tile_to_play)
+
+        expect(self.game.board).not_to(be_empty)
+        expect(self.game.board).to(contain(tile_to_play))
