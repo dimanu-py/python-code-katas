8d25ff1df1a81d07a615a02b057e1fbb8f328456
dimanu-py <diegomtz126@gmail.com>
2024-10-24 13:54:27 +0200

    refactor(test-game): clean up, move Game creation to setup method

--- tic_tac_toe/tests/test_game.py
+++ tic_tac_toe/tests/test_game.py
@@ -6,26 +6,21 @@ from tic_tac_toe.src.invalid_turn import InvalidTurnError
 
 class TestGame:
 
-    def test_player_O_cannot_play_first(sefl):
-        game = Game()
+    def setup_method(self):
+        self.game = Game()
 
-        expect(lambda: game.play(player="O")).to(raise_error(InvalidTurnError))
+    def test_player_O_cannot_play_first(self):
+        expect(lambda: self.game.play(player="O")).to(raise_error(InvalidTurnError))
 
     def test_player_X_starts_playing(self):
-        game = Game()
-
-        expect(lambda: game.play(player="X")).not_to(raise_error(InvalidTurnError))
+        expect(lambda: self.game.play(player="X")).not_to(raise_error(InvalidTurnError))
 
     def test_player_cannot_play_twice_in_a_row(self):
-        game = Game()
+        self.game.play(player="X")
 
-        game.play(player="X")
-
-        expect(lambda: game.play(player="X")).to(raise_error(InvalidTurnError))
+        expect(lambda: self.game.play(player="X")).to(raise_error(InvalidTurnError))
 
     def test_players_alternates_turns(self):
-        game = Game()
+        self.game.play(player="X")
 
-        game.play(player="X")
-
-        expect(lambda: game.play(player="O")).not_to(raise_error(InvalidTurnError))
+        expect(lambda: self.game.play(player="O")).not_to(raise_error(InvalidTurnError))
