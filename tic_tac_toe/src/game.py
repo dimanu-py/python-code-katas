fcec9c6599d8d6a08f41d596ee4b372b6137ed7f
dimanu-py <diegomtz126@gmail.com>
2024-10-24 13:45:45 +0200

    refactor(game): not allow a player to play two turns in a row

--- tic_tac_toe/src/game.py
+++ tic_tac_toe/src/game.py
@@ -4,6 +4,11 @@ from tic_tac_toe.src.player import Player
 
 class Game:
 
+    def __init__(self) -> None:
+        self.player_to_play = Player.O
+
     def play(self, player: str) -> None:
-        if player == Player.O:
+        if player == self.player_to_play:
             raise InvalidTurnError(player)
+
+        self.player_to_play = Player(player)
