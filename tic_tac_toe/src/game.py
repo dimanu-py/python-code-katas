c9f1cad259007b7e48ecc44211f0277850176d25
dimanu-py <diegomtz126@gmail.com>
2024-10-24 15:00:23 +0200

    feat(game): mark tile by player

--- tic_tac_toe/src/game.py
+++ tic_tac_toe/src/game.py
@@ -1,3 +1,5 @@
+from typing import Set, Any
+
 from tic_tac_toe.src.invalid_turn import InvalidTurnError
 from tic_tac_toe.src.player import Player
 from tic_tac_toe.src.tile import Tile
@@ -5,7 +7,11 @@ from tic_tac_toe.src.tile import Tile
 
 class Game:
 
+    player_to_play: Player
+    board: set[Tile]
+
     def __init__(self) -> None:
+        self.board = set()
         self.player_to_play = Player.O
 
     def play(self, player: str, tile: Tile) -> None:
@@ -13,3 +19,4 @@ class Game:
             raise InvalidTurnError(player)
 
         self.player_to_play = Player(player)
+        self.board.add(tile)
