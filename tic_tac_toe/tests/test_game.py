a2d24f2d374841f796eb2eb8323aee42c6bfb660
dimanu-py <diegomtz126@gmail.com>
2024-10-24 14:32:49 +0200

    refactor(game): introduce the concept of Tile in the game

--- tic_tac_toe/tests/test_game.py
+++ tic_tac_toe/tests/test_game.py
@@ -2,6 +2,7 @@ from expects import expect, raise_error
 
 from tic_tac_toe.src.game import Game
 from tic_tac_toe.src.invalid_turn import InvalidTurnError
+from tic_tac_toe.src.tile import Tile
 
 
 class TestGame:
@@ -10,17 +11,17 @@ class TestGame:
         self.game = Game()
 
     def test_player_O_cannot_play_first(self):
-        expect(lambda: self.game.play(player="O")).to(raise_error(InvalidTurnError))
+        expect(lambda: self.game.play(player="O", tile=Tile.TOP_LEFT)).to(raise_error(InvalidTurnError))
 
     def test_player_X_starts_playing(self):
-        expect(lambda: self.game.play(player="X")).not_to(raise_error(InvalidTurnError))
+        expect(lambda: self.game.play(player="X", tile=Tile.TOP_LEFT)).not_to(raise_error(InvalidTurnError))
 
     def test_player_cannot_play_twice_in_a_row(self):
-        self.game.play(player="X")
+        self.game.play(player="X", tile=Tile.TOP_LEFT)
 
-        expect(lambda: self.game.play(player="X")).to(raise_error(InvalidTurnError))
+        expect(lambda: self.game.play(player="X", tile=Tile.TOP_LEFT)).to(raise_error(InvalidTurnError))
 
     def test_players_alternates_turns(self):
-        self.game.play(player="X")
+        self.game.play(player="X", tile=Tile.TOP_LEFT)
 
-        expect(lambda: self.game.play(player="O")).not_to(raise_error(InvalidTurnError))
+        expect(lambda: self.game.play(player="O", tile=Tile.TOP_LEFT)).not_to(raise_error(InvalidTurnError))
