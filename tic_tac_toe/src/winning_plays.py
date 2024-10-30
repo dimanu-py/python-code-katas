47ae90e97fac99903f93cc9b2276f77051ca4035
dimanu-py <diegomtz126@gmail.com>
2024-10-30 21:25:08 +0100

    feat(winning-plays): create WinningPlays that will be in charge of detecting winning situations

---
+++ tic_tac_toe/src/winning_plays.py
@@ -0,0 +1,23 @@
+from tic_tac_toe.src.board import Board
+from tic_tac_toe.src.player import Player
+from tic_tac_toe.src.tile import Tile
+
+
+class WinningPlays:
+
+    def __init__(self, board: Board):
+        self.board = board
+        self.possible_winning_conditions = [
+            [Tile.TOP_RIGHT, Tile.TOP_CENTER, Tile.TOP_LEFT],
+            [Tile.CENTER_RIGHT, Tile.CENTER_CENTER, Tile.CENTER_LEFT],
+            [Tile.BOTTOM_RIGHT, Tile.BOTTOM_CENTER, Tile.BOTTOM_LEFT]
+        ]
+
+    def is_meet_by(self, player: Player) -> bool:
+        return any(
+            self._player_satisfies(winning_play, player)
+            for winning_play in self.possible_winning_conditions
+        )
+
+    def _player_satisfies(self, winning_play: list[Tile], player: Player):
+        return all(self.board.is_marked_by(player, tile) for tile in winning_play)
